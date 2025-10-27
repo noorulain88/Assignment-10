6cda1ac92d099e3eac8f3f40483e9bef7e1fc8db
noorulain88 <noorulainnoorulain86@gmail.com>
2025-10-27 20:37:43 +0500

    assignment 10 program 1-5

---
+++ program.js
@@ -0,0 +1,80 @@
+console.log("hello world")
+
+// logical operater
+
+// program 01
+
+// var username=prompt("enter your name")
+// console.log(" whats ur name",username )
+
+// var userage=prompt("enter your age")
+// console.log(" whats ur age",userage )
+
+// program 02
+ 
+// var studentname=prompt("require student name")
+// console.log(" student name",studentname )
+
+// var rollNo=prompt("enter your roll no")
+// console.log("student roll NO",rollNo )
+
+
+// var grade=prompt("enter your grade")
+// console.log(" student grade",grade)
+
+// program 03
+
+// string
+// var username=prompt("enter your name")
+// console.log(" whats ur name",username )
+
+// number
+// var userage=prompt("enter your age")
+// userage=parseInt(userage)
+// console.log(" whats ur age",userage )
+
+//  boolean
+// var usercourse=prompt("whats ur course" )
+// if(usercourse=="javascript"){
+    
+//     usercourse=true
+// }
+// console.log(" course name",usercourse )  
+
+// program 04
+
+
+// valid
+
+// var class=10
+// console.log(" class in an valid variables",class )
+
+// invalid
+
+// var name1=john
+// console.log(" name is an invalid variablse" , name1)
+
+// program 05
+
+var a=prompt("enter the tha of a ")
+var b=prompt("enter the tha of b ")
+sum=a+b
+console.log("the sum of a and b is")
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
